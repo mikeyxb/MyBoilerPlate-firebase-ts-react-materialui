0957a34adbb630e68dab8cae1f43b94ee94e6fa5
mikeyxb <mr.m.a.brennan@gmail.com>
2024-04-23 23:45:48 +0100

    updated to material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react';
 import './App.css'
 import Auth from './components/auth'
+import { db } from './config/Firebase'
+import { getDocs, collection } from 'firebase/firestore'
+import Button from '@mui/material/Button';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [reviews, setReviews] = useState<{ id: string, guest: string, review: string }[]>([]);
+
+  const reviewsCollectionRef = collection(db, 'reviews');
+
+  // getDocs gets all the documents from the reviews collection
+  // getDoc would get a single document
+
+  useEffect(() => {
+    // this is an async function that will get the reviews from Firestore we use useEffect to run this function when the component mounts, we do this by passing an empty array as the second argument to useEffect.
+    const getReviews = async () => {
+      // READ the reviews collection from Firestore
+
+      //SET the reviews list to the reviews state
+      try {
+        const data = await getDocs(reviewsCollectionRef)
+        const filteredData = data.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+        console.log({ filteredData })
+        setReviews(filteredData as { id: string, guest: string, review: string }[]) // Update the type of filteredData
+      } catch (error) {
+        console.log('Error getting documents: ', error)
+      }
+    };
+    // we call the getReviews function here
+    getReviews();
+  }, []);
 
   return (
     // this is a fragment
     <>
+      <div className='flex flex-row justify-evenly border w-3/4 shadow-2xl rounded-lg overflow-hidden'>
+        <div className='flex p-20 justify-center align-middle w-1/2'>
         <Auth />
+        </div>
+        <div className='flex w-1/2 bg-slate-800 text-white justify-center items-center'>
+          {reviews.map((review) => (
+            <div key={review.id}>
+              <h2>{review.guest}</h2>
+              <p>{review.review}</p>
+
+              <Button variant="outlined" color='error' >Delete</Button>{''}
+
+              <Button variant="contained" endIcon={<ModeEditIcon />}>Edit</Button>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
--- src/components/auth.tsx
+++ src/components/auth.tsx
@@ -1,10 +1,20 @@
 import React, { useState } from 'react'
 import { auth, googleProvider } from '../config/Firebase'
 import { signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth'
+import Button from '@mui/material/Button'
+import { FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField } from '@mui/material'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
 
 const Auth = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
 
     const login = async () => {
         try {
@@ -35,13 +45,43 @@ const Auth = () => {
 
     return (
         <div className='flex flex-col gap-2'>
-            <input type="text" placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
-            <input type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={login}>Login</button>
+            <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password" size='small'>Email</InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    size='small'
+                    label="Email"
+                />
+            </FormControl>
+            <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password" size='small'>Password</InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showPassword ? 'text' : 'password'}
+                    onChange={(e) => setPassword(e.target.value)}
+                    size='small'
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
 
-            <button onClick={signInWithGoogle}>Sign in with Google</button>
+                                edge="end"
+                            >
+                                {showPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                    label="Password"
+                />
+            </FormControl>
+            <Button onClick={login}>Login</Button>
 
-            <button onClick={Logout}>Logout</button>
+            <Button onClick={signInWithGoogle}>Sign in with Google</Button>
+
+            <Button variant='contained' onClick={Logout}>Logout</Button>
         </div>
     )
 }
